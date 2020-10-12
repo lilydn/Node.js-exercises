f9fd8ba6c4b34ef7fd1c6b7216e26442064c29bd
lilydn <kaganli404@gmail.com>
2020-10-12 04:25:08 +0300

    first commit

--- 2-notes-testing/app.js
+++ 2-notes-testing/app.js
@@ -1,34 +1,10 @@
-//what we learned:
-//Importing Node.js core modules (like fs)
-//Importing a function or an object that contains functions from another file
-const notes = require('./notes.js');
-
-const msg = notes.getNotes();
-
-console.log(msg);
-
-//Importing npm modules
-const validator = require('validator');  
-
-console.log(validator.isEmail('lily@gmail.com'));
-console.log(validator.isURL('jdsnfjdsnf.sddsf'));
-
-//print to the console log in green, bold, and inversed with chalk
-const chalk = require('chalk');
-console.log(chalk.green.bold.inverse('Success!'));
-
-//Modules like Chalk and Validate are locally installed dependencies - we need to use require to use them and they are listed in our package files. Globally installed moduled are not listed. 
-// Installing an npm Module Globally: sudo npm i nodemon -g
-
-//get input from the user via arguments provided to the console
-const command = process.argv[2]; //node app.js add
-// console.log(process.argv[2]); //add
-
 // Yargs Package - Adding Command Options
 const yargs = require('yargs');
+const chalk = require('chalk');
+const notes = require('./notes.js');
 
 // Customize yargs version
-yargs.version('1.1.0')
+// yargs.version('1.1.0');
 
 // Create add command
 yargs.command({
@@ -49,7 +25,7 @@ yargs.command({
     handler(argv) {
         notes.addNote(argv.title, argv.body);
     }
-})
+});
 
 // Create remove command
 yargs.command({
@@ -65,7 +41,7 @@ yargs.command({
     handler(argv) {
         notes.removeNote(argv.title);
     }
-})
+});
 
 // Create list command
 yargs.command({
@@ -74,7 +50,7 @@ yargs.command({
     handler() {
         notes.listNotes();
     }
-})
+});
 
 // Create read command
 yargs.command({
@@ -88,8 +64,8 @@ yargs.command({
         }
     },
     handler(argv) {
-        notes.readNote(argv.title)
+        notes.readNote(argv.title);
     }
-})
+});
 
 yargs.parse();
